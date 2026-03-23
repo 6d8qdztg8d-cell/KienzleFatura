bc79f8257e4ab064095b667766b7af8cc86edcdd
altin <tiniolti2006@gmail.com>
2026-03-24 00:46:56 +0100

    fix uninstall-fix.bat: handle both old KienzleFaktura and new KienzleFAT paths

--- uninstall-fix.bat
+++ uninstall-fix.bat
@@ -4,17 +4,23 @@ echo ================================
 
 :: App beenden falls sie läuft
 taskkill /F /IM KienzleFAT.exe 2>nul
+taskkill /F /IM KienzleFaktura.exe 2>nul
 
-:: Alle möglichen Installationsordner löschen
+:: Alle möglichen Installationsordner löschen (alt + neu)
 rmdir /s /q "C:\Program Files\KienzleFAT" 2>nul
 rmdir /s /q "C:\Program Files\KienzleFAT 1.0.0" 2>nul
+rmdir /s /q "C:\Program Files\KienzleFaktura" 2>nul
+rmdir /s /q "C:\Program Files\KienzleFaktura 1.0.0" 2>nul
 rmdir /s /q "C:\Program Files (x86)\KienzleFAT" 2>nul
-rmdir /s /q "C:\Program Files (x86)\KienzleFAT 1.0.0" 2>nul
+rmdir /s /q "C:\Program Files (x86)\KienzleFaktura" 2>nul
 rmdir /s /q "%LOCALAPPDATA%\Programs\KienzleFAT" 2>nul
+rmdir /s /q "%LOCALAPPDATA%\Programs\KienzleFaktura" 2>nul
 
-:: Registry-Einträge löschen
+:: Registry-Einträge löschen (alt + neu)
 reg delete "HKLM\Software\Microsoft\Windows\CurrentVersion\Uninstall\KienzleFAT" /f 2>nul
 reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\Uninstall\KienzleFAT" /f 2>nul
+reg delete "HKLM\Software\Microsoft\Windows\CurrentVersion\Uninstall\KienzleFaktura" /f 2>nul
+reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\Uninstall\KienzleFaktura" /f 2>nul
 
 echo.
 echo Fertig! Jetzt kannst du die neue Version installieren.
